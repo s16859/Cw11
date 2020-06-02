0cb6e4d8661d5a1a9bc9e4aa11c5b9bc54561482
przemek-sz <przemyslawszczerba@interia.pl>
2020-06-02 22:05:35 +0200

    Dodaj pliki projektów.

---
+++ Cw11/Controllers/DoctorController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Cw11.Models;
+using Cw11.DTOs;
+
+namespace Cw11.Controllers
+{
+    [Route("api/doctor")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        private readonly HospitalContext _context;
+
+        public DoctorController(HospitalContext context)
+        {
+            this._context = context;
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult Doctor(int id)
+        {
+            Doctor doctor = _context.Doctors.Find(id);
+
+            if (doctor == null)
+            {
+                return NotFound("Nie znaleziono doctora o id = "+id);
+            }
+
+            return Ok(doctor);
+        }
+
+
+        [HttpPost]
+        public IActionResult Doctor(DoctorDTO doctorDTO)
+        {
+            Doctor doctor = new Doctor();
+
+            doctor.FirstName = doctorDTO.FirstName;
+            doctor.LastName = doctorDTO.LastName;
+            doctor.Email = doctorDTO.Email;
+
+            _context.Doctors.Add(doctor);
+            _context.SaveChanges();
+
+            return Ok(doctor);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateDoctor(DoctorDTO doctorDTO)
+        {
+
+            Doctor doctor = _context.Doctors.Find(Int32.Parse(doctorDTO.IdDoctor));
+
+            if (doctor == null)
+            {
+                return NotFound("Nie znaleziono doctora o id = "+doctorDTO.IdDoctor);
+            }
+
+            doctor.FirstName = doctorDTO.FirstName;
+            doctor.LastName = doctorDTO.LastName;
+            doctor.Email = doctorDTO.Email;
+
+            _context.Update(doctor);
+            _context.SaveChanges();
+
+            return Ok(doctor);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteDoctor(int id)
+        {
+            Doctor doctor = _context.Doctors.Find(id);
+
+            if (doctor == null)
+            {
+                return NotFound("Nie znaleziono doctora o id = "+id);
+            }
+
+            _context.Doctors.Remove(doctor);
+            _context.SaveChanges();
+            
+
+            return Ok();
+        }
+    }
+}
---
+++ Cw11/Migrations/20200602182506_initial.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cw11.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    IdDoctor = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(maxLength: 100, nullable: false),
+                    Email = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.IdDoctor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    IdPatient = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirsName = table.Column<string>(maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(maxLength: 100, nullable: false),
+                    Birthdate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.IdPatient);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prescriptions",
+                columns: table => new
+                {
+                    IdPrescription = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(nullable: false),
+                    DueDate = table.Column<DateTime>(nullable: false),
+                    IdPatient = table.Column<int>(nullable: false),
+                    PatientIdPatient = table.Column<int>(nullable: true),
+                    IdDoctor = table.Column<int>(nullable: false),
+                    DoctorIdDoctor = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prescriptions", x => x.IdPrescription);
+                    table.ForeignKey(
+                        name: "FK_Prescriptions_Doctors_DoctorIdDoctor",
+                        column: x => x.DoctorIdDoctor,
+                        principalTable: "Doctors",
+                        principalColumn: "IdDoctor",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Prescriptions_Patients_PatientIdPatient",
+                        column: x => x.PatientIdPatient,
+                        principalTable: "Patients",
+                        principalColumn: "IdPatient",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Doctors",
+                columns: new[] { "IdDoctor", "Email", "FirstName", "LastName" },
+                values: new object[] { 1, "FerdekKiepski@gmail.com", "Ferdek", "Kiepski" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prescriptions_DoctorIdDoctor",
+                table: "Prescriptions",
+                column: "DoctorIdDoctor");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prescriptions_PatientIdPatient",
+                table: "Prescriptions",
+                column: "PatientIdPatient");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prescriptions");
+
+            migrationBuilder.DropTable(
+                name: "Doctors");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+        }
+    }
+}
